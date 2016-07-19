1fb4e0974ab1a3c442abd67acbc94670ad6174fd
mgreg90 <mgregory8219@gmail.com>
2016-07-19 02:41:59 -0400

    improved functioning of routes and forms

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,21 @@ class SessionsController < ApplicationController
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to group_index_path
+    else
+      redirect_to sessions_new_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to sessions_new_path
   end
 end
